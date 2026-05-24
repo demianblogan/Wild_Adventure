529f950310efc848a79587359fde5dad3b2c6279
Demian Blogan <demianblogan@gmail.com>
2026-05-24 20:07:09 +0200

    Add Game class with fixed-timestep game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,9 @@
-#include <SFML/Graphics.hpp>
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "2D Platformer");
-
-    window.setFramerateLimit(60);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-
-            if (const auto* key = event->getIf<sf::Event::KeyPressed>())
-            {
-                if (key->code == sf::Keyboard::Key::Escape)
-                    window.close();
-            }
-        }
-
-        window.clear(sf::Color(30, 30, 46));
-        window.display();
-    }
+	Game game;
+	game.Run();
 
     return 0;
 }
