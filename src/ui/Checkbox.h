302029f6474a159d658759c211dfb7f902e7b35c
Demian Blogan <demianblogan@gmail.com>
2026-05-29 09:07:17 +0300

    Add UI element Checkbox

---
+++ src/ui/Checkbox.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ui/InteractiveElement.h"
+
+#include <array>
+#include <functional>
+#include <memory>
+
+namespace UI
+{
+	class Checkbox : public InteractiveElement
+	{
+	public:
+		Checkbox();
+
+		void SetBackground(InteractionState state, std::unique_ptr<Element> element);
+		void SetCheckedView(std::unique_ptr<Element> element);
+		void SetUncheckedView(std::unique_ptr<Element> element);
+
+		void SetChecked(bool checked);
+		bool IsChecked() const { return isChecked; }
+
+		void SetOnCheckedChanged(std::function<void(bool)> callback);
+
+	protected:
+		void OnStateChanged() override;
+
+	private:
+		void HandleRelease();
+		void RefreshVisibility();
+
+		Element* GetBackgroundForState(InteractionState state) const;
+
+		std::array<Element*, 3> backgrounds = { nullptr, nullptr, nullptr };
+		Element* checkedView = nullptr;
+		Element* uncheckedView = nullptr;
+
+		bool isChecked = false;
+		std::function<void(bool)> onCheckedChanged;
+	};
+}
